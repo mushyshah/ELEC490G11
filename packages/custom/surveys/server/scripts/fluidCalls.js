0c0a74a0dbe8f4db92cfbdf3f41e69826ce409aa
Mushy Shah <mushyshah@Mushys-MacBook-Pro.local>
2016-02-03 20:34:03 -0500

    Switching to test survey

--- packages/custom/surveys/server/scripts/fluidCalls.js
+++ packages/custom/surveys/server/scripts/fluidCalls.js
@@ -85,45 +85,43 @@ exports.responseCompleted = function(responseid, output){
                     result.completed = 1;
                     result.responseid = responseid;
 
-                    //var OM2r = res.body.7yUn21RPTu;
+                    // result.OM1 = parsed['nP7DrNCUzl'] ;
+                    // result.OM2r = 4-parsed['7yUn21RPTu'] ;
+                    // result.OM3r = 4-parsed['cbIq7Kn8gd'] ;
+                    // result.OM6 = parsed['t1eODgq9GZ'] ;
+                    // result.OMSS = (result.OM1 + result.OM2r + result.OM3r)/3 ;
+                    // result.OMR = result.OM6;
 
-                    result.OM1 = parsed['nP7DrNCUzl'] ;
-                    result.OM2r = 4-parsed['7yUn21RPTu'] ;
-                    result.OM3r = 4-parsed['cbIq7Kn8gd'] ;
-                    result.OM6 = parsed['t1eODgq9GZ'] ;
-                    result.OMSS = (result.OM1 + result.OM2r + result.OM3r)/3 ;
-                    result.OMR = result.OM6;
+                    // result.LB1 = parsed['KMixbRcThI'] ;
+                    // result.LB2 = parsed['PJ4pJtSrPn'] ;
+                    // result.LB3r = 4-parsed['NScbd99MKi'] ;
+                    // result.LB4 = parsed['tPQ2De8LaI'] ;
+                    // result.LB6 = parsed['CGTvWJbSTA'] ;
+                    // result.LBSS = (result.LB1 + result.LB2 + result.LB3r + result.LB4)/4 ;
+                    // result.LBR = result.LB6;
 
-                    result.LB1 = parsed['KMixbRcThI'] ;
-                    result.LB2 = parsed['PJ4pJtSrPn'] ;
-                    result.LB3r = 4-parsed['NScbd99MKi'] ;
-                    result.LB4 = parsed['tPQ2De8LaI'] ;
-                    result.LB6 = parsed['CGTvWJbSTA'] ;
-                    result.LBSS = (result.LB1 + result.LB2 + result.LB3r + result.LB4)/4 ;
-                    result.LBR = result.LB6;
+                    // result.SE1r = 4-parsed['KqLlS9LTKI'] ;
+                    // result.SE2 = parsed['VlqbTTdS9o'] ;
+                    // result.SE3 = parsed['udTJecFeh6'] ;
+                    // result.SE4 = parsed['0ovHVkmbwk'] ;
+                    // result.SE6 = parsed['oAVAhQJTef'] ;
+                    // result.SESS = (result.SE1r + result.SE2 + result.SE3 + result.SE4)/4 ;
+                    // result.SER = result.SE6;
 
-                    result.SE1r = 4-parsed['KqLlS9LTKI'] ;
-                    result.SE2 = parsed['VlqbTTdS9o'] ;
-                    result.SE3 = parsed['udTJecFeh6'] ;
-                    result.SE4 = parsed['0ovHVkmbwk'] ;
-                    result.SE6 = parsed['oAVAhQJTef'] ;
-                    result.SESS = (result.SE1r + result.SE2 + result.SE3 + result.SE4)/4 ;
-                    result.SER = result.SE6;
+                    // result.TR1 = parsed['Dd5GrseoXe'] ;
+                    // result.TR2 = parsed['yvTAGH0p0t'] ;
+                    // result.TR3 = parsed['Jn9Ao7xQ9m'] ;
+                    // result.TR5 = parsed['WRVhpoDzVr'] ;
+                    // result.TRSS = (result.TR1 + result.TR2 + result.TR3)/3 ;
+                    // result.TRR = result.TR5;
 
-                    result.TR1 = parsed['Dd5GrseoXe'] ;
-                    result.TR2 = parsed['yvTAGH0p0t'] ;
-                    result.TR3 = parsed['Jn9Ao7xQ9m'] ;
-                    result.TR5 = parsed['WRVhpoDzVr'] ;
-                    result.TRSS = (result.TR1 + result.TR2 + result.TR3)/3 ;
-                    result.TRR = result.TR5;
-
-                    result.O1 = parsed['hhXNiXRVRk'] ;
-                    result.O2 = parsed['ndNlTZGveV'] ;
-                    result.O3r = 4-parsed['DN8mZnESRy'] ;
-                    result.O4 = parsed['IMXNDQPfz5'] ;
-                    result.O6 = parsed['WCWhpU2JHU'] ;
-                    result.OSS = (result.O1 + result.O2 + result.O3r + result.O4)/4 ;
-                    result.OR = result.O6;
+                    // result.O1 = parsed['hhXNiXRVRk'] ;
+                    // result.O2 = parsed['ndNlTZGveV'] ;
+                    // result.O3r = 4-parsed['DN8mZnESRy'] ;
+                    // result.O4 = parsed['IMXNDQPfz5'] ;
+                    // result.O6 = parsed['WCWhpU2JHU'] ;
+                    // result.OSS = (result.O1 + result.O2 + result.O3r + result.O4)/4 ;
+                    // result.OR = result.O6;
 
 
                           result.save(function(err) {
